558e54c9b8003a7d3dce0bb4df2aab7512ece6dc
mauriceezechieldegnon-wq <mauriceezechieldegnon@gmail.com>
2025-10-07 16:34:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ document.getElementById("devisForm").addEventListener("submit",
         const message = document.getElementById("message").value.trim();  
         if (!nom || !email || !tel || !service || !message) 
             {  alert("Veuillez remplir tous les champs.");    
-                return;  }  const whatsappNumber = "22958660666";  
+                return;  }  
+        const whatsappNumber = "22958660666";  
         const texte = `Bonjour, je m'appelle nom. Je souhaite un devis pour :{service}.Détails : message. Email :{email}, WhatsApp : tel`;  
         const lien = `https://wa.me/{whatsappNumber}?text=${encodeURIComponent(texte)}`;  window.open(lien, "_blank");});
 
